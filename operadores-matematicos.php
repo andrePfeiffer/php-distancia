10e4128233fabb984530765dad2be17ec4935d16
André Pfeiffer <andre.pfeiffer+iparos@gmail.com>
2012-10-23 20:36:59 -0200

    Ensinando operadores matemáticos

---
+++ operadores-matematicos.php
@@ -0,0 +1,44 @@
+<?php
+
+$var1 = 10;
+$var2 = 20;
+$var3 = 30;
+
+// adição
+//echo $var1 + $var2;
+
+// subtração
+//echo $var1 - $var2;
+
+// multiplicação
+// echo $var1 * $var2;
+
+// divisão
+//echo $var1 / $var2;
+
+// módulo (resto da divisão)
+//echo $var1 % 3;
+//echo '<br>';
+//echo $var2 % 3;
+//echo '<br>';
+//echo $var3 % 3;
+
+/* 
+ * ordem das operações
+echo $var1 + $var2 * $var3;
+echo '<br>';
+echo ($var1 + $var2) * $var3;
+ */
+
+/* incremento e decremento
+$var1 = $var1 + 1;
+$var1++;$var1++;$var1++;$var1++;$var1--;$var1--;
+echo $var1;
+ */
+
+
+$var1 = $var1 + 10;
+$var1 += 10;
+$aviso = '';
+$aviso .= 'A minha mensagem de aviso';
+$aviso = $aviso . 'A minha mensagem de aviso';
