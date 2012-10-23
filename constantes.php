9811d72a4136c87583f541b7c9f1fb4dacac2681
André Pfeiffer <andre.pfeiffer+iparos@gmail.com>
2012-10-23 13:30:40 -0200

    Ensinando constantes

---
+++ constantes.php
@@ -0,0 +1,24 @@
+<?php
+
+define('CONSTANTE', 'Ola mundo!', true);
+
+echo CONSTANTE;
+echo '<br>';
+echo constante;
+echo '<br>';
+echo CoNsTaNtE;
+
+echo '<br>';
+echo '<hr>';
+echo '<br>';
+
+echo __FILE__;
+echo ':';
+echo __LINE__;
+
+/*
+ * __FUNCTION__
+ * __CLASS__
+ * __METHOD__
+ * 
+ */
