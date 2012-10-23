bffde519ea84650797b0ae6609c25c2a436376c0
André Pfeiffer <andre.pfeiffer+iparos@gmail.com>
2012-10-23 20:24:54 -0200

    Ensinando comentários

---
+++ comentarios.php
@@ -0,0 +1,26 @@
+<?php
+
+// comentario de uma linha
+
+/*
+ * comentario de mais
+ * de uma
+ * linha
+ */
+
+/*
+comentario de mais
+de uma
+linha
+ */
+
+
+/**
+ * 
+ * @param type $var1
+ * @param type $var2
+ * @return boolean
+ */
+function nome_da_funcao($var1, $var2){
+    return true;
+}
