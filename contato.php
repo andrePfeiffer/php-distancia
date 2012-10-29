0eb4c265792dc59a4d1121649422d6655204fc2a
André Pfeiffer <andre.pfeiffer+iparos@gmail.com>
2012-10-29 21:29:26 -0200

    Validando os dados

--- contato.php
+++ contato.php
@@ -1,17 +1,35 @@
-<?php 
-if(!empty($_POST['nome'])){
-    $mensagem = '';
-    $mensagem .= 'Obrigado por entrar em contato conosco!<br />';
+<?php
+
+
+if (isset($_POST['nome'])) {
+    $aviso = '';
     
-    $mensagem .= $_POST['nome'] . '<br />';
-    $mensagem .= $_POST['email'] . '<br />';
-    $mensagem .= $_POST['area'] . '<br />';
-    $mensagem .= $_POST['mensagem'] . '<br />';
+    $nome = $_POST['nome'];
+    $email = $_POST['email'];
+    $area = $_POST['area'];
+    $mensagem = $_POST['mensagem'];
     
-    $mensagem .= '<br /><a href="formulario.html">voltar</a>';
+    if(empty($nome)){
+        $aviso .= 'Favor preencher o nome<br />';
+    }
+    if(empty($email)){
+        $aviso .= 'Favor preencher o email<br />';
+    }
+    if(empty($area)){
+        $aviso .= 'Favor preencher a area<br />';
+    }
+    if(empty($mensagem)){
+        $aviso .= 'Favor preencher a mensagem<br />';
+    }
+    
+    if(empty($aviso)){
+        $aviso .= 'Recebemos sua mensagem. Dados enviados<br />';
+        $aviso .= 'Nome: ' . $nome . '<br />';
+        $aviso .= 'Email: ' . $email . '<br />';
+        $aviso .= 'Mensagem: ' . $mensagem . '<br />';
+    }
 }else{
-    $mensagem = 'Favor preencher corretamente o formulário<br />';
-    $mensagem .= '<a href="formulario.html">voltar</a>';
+    $aviso = 'Você não enviou o formulário';
 }
 ?>
 <!DOCTYPE html>
@@ -21,6 +39,6 @@ if(!empty($_POST['nome'])){
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     </head>
     <body>
-        <?php echo $mensagem; ?>
+        <?php echo $aviso; ?>
     </body>
 </html>
