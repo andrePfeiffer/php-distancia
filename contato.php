3c9ef224770a648ce8bb285483198889eb037750
André Pfeiffer <andre.pfeiffer+iparos@gmail.com>
2012-10-29 20:45:07 -0200

    Criando o formulario de contato

---
+++ contato.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title></title>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    </head>
+    <body>
+        Obrigado por entrar em contato conosco!
+    </body>
+</html>
