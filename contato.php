8b75a9d9ea3c9454a55b56f39329d13a85dfdcb3
André Pfeiffer <andre.pfeiffer+iparos@gmail.com>
2012-10-29 21:04:22 -0200

    Testando o recebimento no PHP

--- contato.php
+++ contato.php
@@ -1,3 +1,19 @@
+<?php 
+if(!empty($_POST['nome'])){
+    $mensagem = '';
+    $mensagem .= 'Obrigado por entrar em contato conosco!<br />';
+    
+    $mensagem .= $_POST['nome'] . '<br />';
+    $mensagem .= $_POST['email'] . '<br />';
+    $mensagem .= $_POST['area'] . '<br />';
+    $mensagem .= $_POST['mensagem'] . '<br />';
+    
+    $mensagem .= '<br /><a href="formulario.html">voltar</a>';
+}else{
+    $mensagem = 'Favor preencher corretamente o formulário<br />';
+    $mensagem .= '<a href="formulario.html">voltar</a>';
+}
+?>
 <!DOCTYPE html>
 <html>
     <head>
@@ -5,13 +21,6 @@
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     </head>
     <body>
-        Obrigado por entrar em contato conosco!
-        <br />
-        <?php if(isset($_POST['nome'])){ ?>
-        <?php echo $_POST['nome']; ?><br />
-        <?php echo $_POST['email']; ?><br />
-        <?php echo $_POST['area']; ?><br />
-        <?php echo $_POST['mensagem']; ?><br />
-        <?php } ?>
+        <?php echo $mensagem; ?>
     </body>
 </html>
