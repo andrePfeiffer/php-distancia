dcbce66d0e4b5eb0d104d5b3628249acc72a5feb
André Pfeiffer <andre.pfeiffer+iparos@gmail.com>
2012-11-15 02:15:11 -0200

    Arrays - parte 2

--- arrays.php
+++ arrays.php
@@ -32,3 +32,19 @@ $meses = array(
 
 echo $dias_da_semana[date('w')] . ', ' . date('d') . ' de ' . $meses[date('m')-1] . ' de ' . date('Y');
 
+echo '<br><hr><br>';
+
+echo '<pre>';
+print_r($meses);
+print_r($meses[0]);
+echo '</pre>';
+
+echo '<br><hr><br>';
+
+echo '<pre>';
+var_dump($meses);
+var_dump($meses[0]);
+echo '</pre>';
+
+
+
