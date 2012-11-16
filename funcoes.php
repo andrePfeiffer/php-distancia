2f71bf59e66bb38a85b42e9f11dd6696dc5908b0
André Pfeiffer <andre.pfeiffer+iparos@gmail.com>
2012-11-16 19:46:21 -0200

    Funções no PHP

---
+++ funcoes.php
@@ -0,0 +1,13 @@
+<?php
+
+function texto_em_negrito($texto){
+    return '<strong>' . $texto . '</strong><br />';
+}
+
+
+echo texto_em_negrito('oi');
+echo texto_em_negrito('tudo');
+echo texto_em_negrito('bem');
+echo texto_em_negrito('como');
+echo texto_em_negrito('vai');
+
