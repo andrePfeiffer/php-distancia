b011d09cfe3e3e5db33c24ceaeadbc3d9826bdb4
André Pfeiffer <andre.pfeiffer+iparos@gmail.com>
2012-11-21 18:12:49 -0200

    Data por extenso - parte 1

--- contador.php
+++ contador.php
@@ -4,6 +4,27 @@ header('Content-Type: text/html; charset=utf-8');
 $fim_do_mundo = mktime(0, 0, 0, 12, 21, 2012);
 $fim_do_mundo_segundos = $fim_do_mundo - time();
 
-echo date('d/m/Y', $fim_do_mundo);
+function data_por_extenso($time){
+    $dias_da_semana = array('Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado');
+    $meses = array(
+                'Janeiro',
+                'Fevereiro',
+                'Março',
+                'Abril',
+                'Maio',
+                'Junho',
+                'Julho',
+                'Agosto',
+                'Setembro',
+                'Outubro',
+                'Novembro',
+                'Dezembro',
+            );
+
+
+    return date('d', $time) . ' de ' . $meses[date('m', $time)-1] . ' de ' . date('Y', $time);
+}
+
+echo data_por_extenso($fim_do_mundo);
 echo '<br>';
 echo 'Faltam ' . number_format($fim_do_mundo_segundos, 0, ',', '.') . ' segundos para o fim do mundo';
