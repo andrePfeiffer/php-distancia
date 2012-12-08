5971f832447ccd50d1359b3ad08c7402462cc450
André Pfeiffer <andre.pfeiffer+iparos@gmail.com>
2012-12-08 16:00:02 -0200

    Segunda versão do servidor.php

--- servidor.php
+++ servidor.php
@@ -12,17 +12,13 @@ if($_SERVER['SERVER_NAME'] == 'localhost' || $_SERVER['SERVER_NAME'] == '127.0.0
     define('DB_BASE', 'ctiparos_phponline', true);
 }
 
-mysql_connect(DB_HOST, DB_USER, DB_PASS) or die(mysql_error());
-mysql_select_db(DB_BASE) or die(mysql_error());
+$db_link = mysqli_connect(DB_HOST, DB_USER, DB_PASS) or die(mysql_error());
+mysqli_select_db($db_link, DB_BASE);
+
 
 function consulta_dados($query){
-    $resultado = mysql_query($query) or die(mysql_error());
+    global $db_link;
+    $resultado = mysqli_query($db_link, $query);
     return $resultado;
-}
-
-$query = consulta_dados("select * from livros");
-
-while($livros = mysql_fetch_array($query)){
-    echo $livros['titulo'] . '<br />';
 }
 
