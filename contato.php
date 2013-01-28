5e1be961d0dd4d441d9307bdf4a268b9189ccfcb
André Pfeiffer <andre.pfeiffer+iparos@gmail.com>
2013-01-28 04:38:12 -0200

    Finalizando o formulário de contato

--- contato.php
+++ contato.php
@@ -17,20 +17,46 @@ if (isset($_POST['nome'])) {
     }
     if(empty($area)){
         $aviso .= 'Favor preencher a area<br />';
+    }else{
+        if($area == 'vendas'){
+            $email_to = 'vendas@iparos.com.br';
+        }elseif($area == 'suporte'){
+            $email_to = 'suporte@iparos.com.br';
+        }else{
+            $aviso .= 'Área seleciona é inválida<br />';
+        }
     }
     if(empty($mensagem)){
         $aviso .= 'Favor preencher a mensagem<br />';
     }
     
-    if(empty($aviso)){
-        $aviso .= 'Recebemos sua mensagem. Dados enviados<br />';
-        $aviso .= 'Nome: ' . $nome . '<br />';
-        $aviso .= 'Email: ' . $email . '<br />';
-        $aviso .= 'Mensagem: ' . $mensagem . '<br />';
+    if(empty($aviso) && isset($email_to)){
+        $body = '
+        <!DOCTYPE html>
+        <html>
+            <head>
+                <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+            </head>
+            <body>
+                Email enviado através do site<br />';
+        $body .= date('d/m/Y h:m:s') . '<br />';
+        $body .= 'Nome: ' . $nome . '<br />';
+        $body .= 'Email: ' . $email . '<br />';
+        $body .= 'Mensagem:<br />' . nl2br($mensagem) . '<br />';
+        $body .= '</body></html>';
         
-        $aviso .= '<br /><br /><a href="contato.php">voltar</a>';
+        $email_headers = "Content-type: text/html;charset=utf-8;";
+        $email_headers .= "\r\nFrom:" . $email;
         
-        $sucesso = true;
+        $retorno = mail($email_to, 'Email enviado através do formulário de contato', $body, $email_headers);
+        
+        if($retorno){
+            $sucesso = true;
+            $aviso = 'Email enviado com sucesso';
+        }else{
+            $aviso = 'Erro ao enviar email, favor tentar mais tarde<br />';
+        }
+        
     }
 }
 ?>
